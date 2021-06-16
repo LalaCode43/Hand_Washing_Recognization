691992803f80edde47ab7d09267470bea8046ebd
LaLaCode43 <LaLaCode43@gmail.com>
2021-06-16 14:24:34 +0700

    predict and reslt

--- config.py
+++ config.py
@@ -5,13 +5,13 @@ np.random.seed(4)
 random.seed(4)
 root_path = '/media/vas/54D6ABF9D6ABDA0E/Documents and Settings/data'
 resize = 224
-# mean = (0.485, 0.456, 0.406)
-# std = (0.229, 0.224, 0.225)
+mean = (0.485, 0.456, 0.406)
+std = (0.229, 0.224, 0.225)
 batch_size = 64
 num_epochs = 30
 num_classes = 7
 # mean = [0.4294, 0.4446, 0.4264]
 # std = [0.2134, 0.2001, 0.1820]
-mean  = [0.4172, 0.4346, 0.4198]
-std = [0.1786, 0.1957, 0.2089]
+# mean  = [0.4172, 0.4346, 0.4198]
+# std = [0.1786, 0.1957, 0.2089]
 
--- hand_washing.py
+++ hand_washing.py
@@ -21,7 +21,7 @@ def parse_inputs():
         '--weight_dir',
         metavar='W',
         type=str,
-        default='../weights/mobilenet_12.pth'
+        default='./result/14_6_2021/weights/mobilenet_28.pth'
     )
     
     parser.add_argument(
@@ -73,29 +73,88 @@ def main(args):
     
     # softmax function 
     sm = nn.Softmax(dim=1)
+    
+    # transform image
+    transform = ImageTransform(resize, mean, std)
+    
+    
 
-    if file_dir.split('.')[-1] == 'jpg': # if input is image
+    if file_dir.split('.')[-1] == 'jpg': # if input is image file
         # read image
         img = cv2.imread(file_dir)
         frame = img[:, :, (2, 1, 0)]
         frame = Image.fromarray(frame)
     
-        transform = ImageTransform(resize, mean, std)
+        
         
         
         img_transformed = transform(frame, 'val')
+        print(img_transformed.shape)
+        img1 = img_transformed.numpy().transpose(1, 2, 0)
+        img1 = np.clip(img1, 0, 1)
         img_transformed = img_transformed.unsqueeze_(0)
         img_transformed = img_transformed.to(device)
-
+    
         output = sm(net(img_transformed))
         print(output)
         ret, preds = torch.max(output, dim=1)
-        print(preds.item() + 1)
+        print(preds.item())
         cv2.imshow('img', img)
         cv2.waitKey(0)        
+        cv2.imshow('img', img1)
+        cv2.waitKey(0)    
+    if file_dir.split('.')[-1] in ['mp4', 'avi']: # if input is video file
+        vidcap = cv2.VideoCapture(file_dir)
+        class_total = np.zeros(7, dtype=int)
+        width = int(vidcap.get(3))
+        height = int(vidcap.get(4))
+        fourcc = cv2.VideoWriter_fourcc('M', 'J', 'P', 'G')
+        FILE_OUTPUT = '../output.avi'
+        out = cv2.VideoWriter(FILE_OUTPUT, fourcc, 30, (int(width), int(height)))
+        while True:
+            
+            ret, frame_cv = vidcap.read() 
         
-        
+            if ret:
+                frame = frame_cv[:, :, (2, 1, 0)]
+                img = Image.fromarray(frame)
+                
+                input_frame  =  transform(img, 'val')
+                
+                input_frame = input_frame.to(device)
+                
+                input_frame = input_frame.unsqueeze_(0)
+                
+                output_frame = net(input_frame)          
+                prob_outs = sm(output_frame)
+                
+                value, index = torch.max(prob_outs, dim=1)
+                
+                pvalue = value.cpu().item()
+                pclass = index.cpu().item()
+                class_total[pclass] += 1
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                cv2.putText(frame_cv,
+                                    str(pclass),
+                                    (50, 50),
+                                    font, 1,
+                                    (0, 255, 255),
+                                    2,
+                                    cv2.LINE_4)
+                cv2.imshow('my webcam', frame_cv)
+                out.write(frame_cv)
 
+                if cv2.waitKey(1) == ord('q'):
+                    break
+            else:
+                break
+        vidcap.release()
+        result.release()
+        cv2.destroyAllWindows()
+        print("The video was successfully saved")
+        print('Total of step: ', class_total)
+    
+                
 if __name__ == '__main__':
     args = parse_inputs()
     main(args)
